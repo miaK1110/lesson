5bfeee82a6df7416a6298f1d1788cd1a024b001a
mia <m.09048030369@gmail.com>
2023-11-21 06:35:11 +0900

    commit

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,19 @@ export type Todo = {
   created_at: string
   title: string
 }
+export type AuthForm = {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  confirmPassword: string
+  termsOfService: boolean
+}
+export type Post = {
+  id: number
+  created_at: string
+  title: string
+  content: string
+  post_url: string
+  status: string
+}
